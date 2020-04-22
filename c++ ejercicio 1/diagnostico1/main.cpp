b30803e69f04bd01e2005f702d52b91b6957a1cb
Coprigby <33013710+Coprigby@users.noreply.github.com>
2020-04-22 15:24:01 -0400

    update maincpp
    Se agregaron comentarios al codigo

--- c++ ejercicio 1/diagnostico1/main.cpp
+++ c++ ejercicio 1/diagnostico1/main.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 void devContLetr(string palabra){
 
-
+    //Se pasa toda la palabra a minusculas
     std::for_each(palabra.begin(), palabra.end(), [](char & c){
 	c = ::tolower(c);
     });
 
-
+    //se inicializa un booleano para que en caso de tener caracteres que no sean letras no continue
     bool continua = true;
     int i = 0;
-
+	
+    //ciclo for para comprobar que los caracteres se encuentren en el rango ascii de las letras
     for(i=0;i<palabra.length();i++){
         if(continua == false){
             break;
@@ -26,20 +27,26 @@ void devContLetr(string palabra){
         }
     }
 
+    
     string resultado = "";
     int cantidad = 0;
+    
+    //condicion para continuar si solo contiene letras
     if(continua == true){
         for(i=0;i<palabra.length();i++){
-
+	   
+            //si el ciclo va en la segunda letra y ademas es igual a la anterior se suma 1 a la cantidad de veces que se repite
             if((i!=0) && (palabra[i] == palabra[i-1])){
                 cantidad += 1;
-
             }
+	    //si la letra es distinta de la anterior y ademas no corresponde a la primera letra de la palabra se suma esta misma y se agrega al string resultado
             if((palabra[i] != palabra[i-1])&&(i!=0)){
                 cantidad += 1;
                 resultado += palabra[i-1] + to_string(cantidad);
+	        //se vuelve la cantidad a 0
                 cantidad = 0;
             }
+	    //Este if es para cuando este en la ultima letra ya que el primer if no sirve debido a que se pasaria el tope y apuntaria fuera del "arreglo" palabra
             if(i == (palabra.length()-1)){
                 cantidad += 1;
                 resultado += palabra[i] + to_string(cantidad);
@@ -49,6 +56,7 @@ void devContLetr(string palabra){
     if(continua == false){
         cout << "La palabra debe contener solo letras" << endl;
     }
+    //Imprime el resultado que serian las letras+cantidad de veces que se encuentran formato LetraCantidadLCLCLCLCLC....
     cout << resultado << endl;
 }
 
@@ -57,9 +65,10 @@ int main()
 {
     while(1==1){
         string Palabra;
+	//palabra a trabajar se pide por teclado
         cout << "Ingrese la palabra a trabajar: ";
         cin >> Palabra;
-
+	
         devContLetr(Palabra);
         system("pause");
         system("CLS");
